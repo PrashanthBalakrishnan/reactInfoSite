68182793bbe07b1c83547ae9dfac216cad2d3606
unknown <prashanthdilshaan@gmail.com>
2022-05-20 17:14:10 -0400

    set up react

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Main from "./components/Main"
+import NavBar from "./components/Navbar"
+
+export default function App() {
+    return (
+        <div className="container">
+            < Main />
+            < NavBar />
+        </div>
+    )
+}
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+export default function NavBar() {
+    return <h1>Nav Bar goes here</h1>
+
+}
