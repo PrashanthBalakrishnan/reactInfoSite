b4e1d9975dcc1cd574acc0802fc75ddae7f55bee
unknown <prashanthdilshaan@gmail.com>
2022-07-11 15:09:37 -0400

    added Dark mode button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import Main from "./components/Main"
 import NavBar from "./components/Navbar"
 
+
 export default function App() {
+
+    const [darkMode, setDarkMode] = useState(true)
+
+    function toggleDarkMode() {
+        setDarkMode(currentMode => !currentMode)
+    }
+
     return (
         <div className="container">
-            < NavBar />
-            < Main />
+            < NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            < Main darkMode={darkMode} />
         </div>
 
     )
